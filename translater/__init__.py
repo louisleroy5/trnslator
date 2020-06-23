4fbf41dea2fea21d01d905e4168bd74fdd78876b
Unknown <louis.leroy5@gmail.com>
2020-06-23 11:26:58 -0400

    Bump version to 1.0.1

--- translater/__init__.py
+++ translater/__init__.py
@@ -6,7 +6,7 @@
 ################################################################################
 
 # Version of the package
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 # warn if a newer version of translater is available
 from outdated import warn_if_outdated
