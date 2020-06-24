dfdf384a401e46ab033ee3946770583d00bd2edd
Unknown <louis.leroy5@gmail.com>
2020-06-24 15:22:28 -0400

    Test rotate()
    increase coverage

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,6 +3,16 @@ import pytest
 from translater import utils
 
 
+def test_rotate(config):
+    # Shift list elements to the left
+    l1 = [1, 2, 3]  # list
+    n = 1  # shift 1 position to the left
+    l2 = utils.rotate(l1, n)
+
+    assert (l2 == [2, 3, 1])
+
+
+
 
 
 
